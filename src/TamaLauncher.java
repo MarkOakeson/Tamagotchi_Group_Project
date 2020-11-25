ca2278fe3283d66a5db3d770c8bc6bc8e3f6305f
Jared Malooly <jared.malooly@gmail.com>
2020-11-25 16:45:25 -0700

    Basic Launcher. Moved path to 1.8

--- src/TamaLauncher.java
+++ src/TamaLauncher.java
@@ -1,9 +1,35 @@
+import java.io.IOException;
+import javafx.application.Application;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+/**
+ * 
+ * @author jmalooy and blsmith86
+ *
+ * Purpose: 
+ */
 public class TamaLauncher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		
+		// Create model and controller objects
+		TamaModel      model      = new TamaModel();
+		TamaController controller = new TamaController(model);
+		
+		// Launch view
+		Application.launch(TamaView.class, args);
+		
 
-	}
+		
+		
+		
+		
+		
+	} // End main
+} // End class
 
-}
