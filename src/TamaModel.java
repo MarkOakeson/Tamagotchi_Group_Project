8a3879c6d93302a415d7a2c3b592566627ddea15
bryankuo1 <bryankuo1@email.arizona.edu>
2020-11-25 18:20:16 -0700

    Added getters; changed "sick" field to "healthy"

--- src/TamaModel.java
+++ src/TamaModel.java
@@ -12,20 +12,20 @@
  * influenced by weight and random chance. If health gets too low, Tamaclonechi can get sick. 
  * Sickness severely impacts a Tamaclonechi's ability to stay alive. To cure sickness, the user 
  * must feed Tamaclonechi medicine. 
- * 		Affects: sick (large effect)
+ * 		Affects: healthy (large effect)
  * 		Affected by: weight, random chance
  * 
  * Weight: Automatically decreases with time and increases when user feeds Tamaclonechi with a 
  * meal or snack. Meals increase weight and happiness slightly; snacks increase weight and 
  * happiness greattly. When over/underweight, Tamaclonechi is more likely to get sick. Also affects
  * health. 
- * 		Affects: health (medium effect), sick (medium effect for over/underweight)
+ * 		Affects: health (medium effect), healthy (medium effect for over/underweight)
  * 		Affected by: meal (large effect), snack (medium effect)
  * 
  * Happiness: When high, Tamaclonechi tends to get sick less and live longer. When low, vice versa.
  * Sickness makes happiness go down. Snacks, meals, and healthiness make Tamaclonechi happier.
- * 		Affects: sick (medium effect), alive (large effect)
- * 		Affected by: meal, snack, sick (large effect), not sick(small effect)
+ * 		Affects: healthy (small effect), alive (large effect)
+ * 		Affected by: meal, snack, healthy (medium effect), not healthy(large effect)
  * 
  * Fullness(?): Increases when max value is 0, decreases weight and happiness when at 0. Not 
  * included in core mechanics, so inclusion of this stat is up for debate. 
@@ -38,7 +38,7 @@ public class TamaModel {
 	private int happiness; 
 	//private float fullness; 
 	
-	private boolean sick;
+	private boolean healthy;
 	private boolean alive;
 	
 	public TamaModel() {
@@ -47,7 +47,14 @@ public class TamaModel {
 		this.weight = 100;
 		this.happiness = 100;
 		//this.fullness = 100;
-		this.sick = false;
+		this.healthy = false;
 		this.alive = true;
 	}
+	
+	public float getAge() {return age;}
+	public int getHealth() {return health;}
+	public int getWeight() {return weight;}
+	public int getHappiness() {return happiness;}
+	public boolean isHealthy() {return healthy;}
+	public boolean isAlive() {return alive;}
 }
