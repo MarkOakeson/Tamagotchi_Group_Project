770f3e18ea5c6414f21430806d053ee8897f0717
Jared Malooly <jared.malooly@gmail.com>
2020-11-25 18:54:27 -0700

    Fixed javafx preferences, blank window, launcher

--- src/TamaView.java
+++ src/TamaView.java
@@ -1,2 +1,49 @@
-public class TamaView {
+import java.util.Observable;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class TamaView extends Application{
+
+	private Observable observable;
+
+	private TamaController controller;
+	private TamaModel model;
+	
+	private BorderPane window;
+	private float height = 500;
+	private float width = 500;
+	
+	
+	
+
+	
+	public TamaView() {
+		this.window = new BorderPane();
+		this.model = new TamaModel();
+		this.controller = new TamaController(this.model);
+	}
+	
+	/**
+	 * Purpose: Creates the gameboard. Includes everything to construct the
+	 * gameboard and gather event data.
+	 * 
+	 * @param object to create the stage
+	 */
+	@Override
+	public void start(Stage stage) {
+		
+		stage.setTitle("Tamagotchi"); // Name the stage
+		
+		Scene scene = new Scene(window, width, height);
+
+		stage.setScene(scene);
+		stage.show(); // Show the stage
+	}
+
+	
+	
+	
 }
