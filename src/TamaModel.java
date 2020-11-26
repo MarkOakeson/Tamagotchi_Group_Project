574deff962a6497f1d0bb7ab8ec2d1d8f8e442d9
bryankuo1 <bryankuo1@email.arizona.edu>
2020-11-25 17:27:30 -0700

    Added basic stats

--- src/TamaModel.java
+++ src/TamaModel.java
@@ -1,2 +1,20 @@
+
+/*
+ * Stat Notes: 
+ * Age: 
+ */
 public class TamaModel {
+	private float age; //Might want to make a Tamaclonechi class and move these there
+	private int health; 
+	private int weight;
+	private int happiness; 
+	private float fullness; //When this goes above 100, increase weight. Decrease when below
+	
+	public TamaModel() {
+		this.age = 0;
+		this.health = 100;
+		this.weight = 100;
+		this.happiness = 100;
+		this.fullness = 100;
+	}
 }
