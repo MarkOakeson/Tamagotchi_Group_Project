92d6da699def691acb5dc396d55c5e0f40e5754b
Jared Malooly <61917780+jared-malooly@users.noreply.github.com>
2020-11-27 18:09:46 -0700

    Revert "6 file management"

--- src/TamaLauncher.java
+++ src/TamaLauncher.java
@@ -1,23 +1,9 @@
 
-import javafx.application.Application;
-
-
-/**
- * 
- * @author 	Aidan McArthur
- *			Bryan Kuo
- *			Jared Malooly
- *			Mark Oakeson
- *
- * Purpose: Launch program
- */
 public class TamaLauncher {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		// Launch view
-		Application.launch(TamaView.class, args);
-		
-	} // End main
-} // End class
+	}
 
+}
